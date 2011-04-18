d28e2a4c6d113cac9d8a8c71ed5e0ecf5c38e187
Toni Alatalo <toni playsign>
2011-04-18 16:10:34 +0300

    little notes & tweaks & example screenshot, the actually in-use runtime version is in the lvm repo now, to be checked later if is the same

--- runtime/hello.js
+++ runtime/hello.js
@@ -29,6 +29,7 @@ console commands used to create the graph and save the graph in editor:
 g = graph.graph
 xdata = {'id': 'x', 'name': 'node x'}
 x = g.addNode(ndata)
+p2 = g.addNode({'id': 'path2', 'name': 'path2'})
 n1 = g.getNode('graphnode1')
 g.addAdjacence(x, n1)
 
@@ -39,4 +40,29 @@ JSON.stringify(graph.toJSON("graph"))
 map.png center: 1086, 571
 
 function addposdata(n) { n.data.posx = n.pos.x; n.data.posy = n.pos.y; }
+g.eachNode(addposdata)
+
+offset calc:
+origo:
+-1.0648120130503267, -54.95458387873319
+==>
++x: 1.0648120130503267
++y: 54.95458387873319
+
+topleft: 
+graph:
+x: -1014.0854424181147
+y: -449.01010552874095
+
+scene:
+-113
+44
+
+==>
+(-1014.0854424181147 + 1.0648120130503267) / -113
+/x: 8.964784339867826
+
+(-449.01010552874095 + 54.95458387873319) / 44
+/y: -8.95580731022745
+
 */
